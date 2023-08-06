243177a6890c3219130e6b63241cbc62adc40a9d
Yato <yatoakii@gmail.com>
2023-08-06 12:26:47 +0630

    setup: threeJS

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css"
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Setup
 
-setupCounter(document.querySelector('#counter'))
+const canvas = document.querySelector("canvas.webgl")
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1,100)
+scene.add(camera)
+
+
+
+
+// Render
+
+const renderer = new THREE.WebGLRenderer({
+  canvas:canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const clock = new THREE.Clock()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  console.log(elapsedTime)
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+
+tick()
